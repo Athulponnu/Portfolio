020e6bc6dbd0a15fb9ad504de11044e90399aef3
Athul <152899418+Athulponnu@users.noreply.github.com>
2025-10-14 09:02:29 +0530

    Made project cards clickable

--- src/Sections/Project.jsx
+++ src/Sections/Project.jsx
@@ -13,24 +13,35 @@ export default function Projects() {
       description: "AI & ML Features Implemented",
       img: Python,
       color: "from-purple-800 to-violet-700",
+      link: "https://github.com/Athulponnu/Click-Counter",
     },
     {
       title: "Android Development (Click Counter)",
       description: "Interactive UI & UX",
       img: apk,
       color: "from-blue-800 to-cyan-700",
+      link: "https://github.com/Athulponnu/IDE",
     },
     {
       title: "Handwritten Digit Recognition",
       description: "Python + Tkinter GUI",
       img: Digit,
       color: "from-gray-800 to-gray-600",
+      link: "https://github.com/Athulponnu/Athulponnu-digit_recognizer_With_UI",
     },
     {
       title: "Personal Portfolio Site",
       description: "React + TailwindCSS",
       img: port,
       color: "from-pink-800 to-purple-600",
+      link: "https://github.com/Athulponnu/Portfolio",
+    },
+    {
+      title: "Theft Detection",
+      description: "YOLO, IoT Integration, Computer Vision",
+      img: port,
+      color: "from-red-800 to-purple-600",
+      link: "https://github.com/Athulponnu/Theft_Detection",
     },
   ];
 
@@ -42,12 +53,15 @@ export default function Projects() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 fade">
         {projects.map((proj, index) => (
-          <motion.div
+          <motion.a
             key={index}
-            className={`rounded-3xl overflow-hidden shadow-xl cursor-pointer bg-gradient-to-br ${proj.color}`}
+            href={proj.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`block rounded-3xl overflow-hidden shadow-xl bg-gradient-to-br ${proj.color}`}
             initial={{ scale: 1 }}
-            whileHover={{ scale: 1.08 }}
-            transition={{ duration: 0.5, ease: "easeOut" }}
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.4, ease: "easeOut" }}
           >
             <img
               src={proj.img}
@@ -55,10 +69,12 @@ export default function Projects() {
               className="w-full h-64 object-cover"
             />
             <div className="p-6">
-              <h2 className="text-3xl font-bold text-white mb-2">{proj.title}</h2>
+              <h2 className="text-3xl font-bold text-white mb-2">
+                {proj.title}
+              </h2>
               <p className="text-xl text-gray-200">{proj.description}</p>
             </div>
-          </motion.div>
+          </motion.a>
         ))}
       </div>
     </main>
